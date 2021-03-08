f8741c3f1d832c3115bd3780dab98e7778f9449d
John-Maravelis <47307883+John-Maravelis@users.noreply.github.com>
2021-03-08 09:05:23 +0200

    Moodle files

---
+++ lang/en/block_showusersandcourses.php
@@ -0,0 +1,8 @@
+<?php
+
+$string['showusersandcourses:addinstance'] = 'Add a new block';
+$string['showusersandcourses:myaddinstance'] = 'Add a new block to the dashboard';
+$string['showusersandcourses'] = 'show active users or active courses';
+$string['pluginname'] = 'show active users or active courses';
+$string['showcourses'] = 'Show active courses';
+$string['showcoursesinfo'] = 'Show the active courses instead of active users';
